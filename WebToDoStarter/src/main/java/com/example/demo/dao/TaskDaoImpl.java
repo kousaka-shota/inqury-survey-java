ba7508d1d4e55d7cc9e6e203d3e1cbc933b66d12
kosaka_shota <shiyota_kosaka@mail.toyota.co.jp>
2025-03-07 09:35:33 +0900

    add tasktalimple

--- WebToDoStarter/src/main/java/com/example/demo/dao/TaskDaoImpl.java
+++ WebToDoStarter/src/main/java/com/example/demo/dao/TaskDaoImpl.java
@@ -6,6 +6,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import javax.swing.text.html.parser.DTD;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -13,132 +16,91 @@ import com.example.demo.entity.Task;
 import com.example.demo.entity.TaskType;
 
 @Repository
-public class TaskDaoImpl implements TaskDao {
+public class TaskDaoImpl implements TaskDao{
 
-	private final JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
-	public TaskDaoImpl(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
-	}
+    @Autowired
+    public TaskDaoImpl(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+    
+    @Override
+    public List<Task> findAll(){
+        String sql = "SELECT * FROM task INNER JOIN task_type ON task.type_id = task_type.id";
+        List<Map<String,Object>> resultList = jdbcTemplate.queryForList(sql);
+        ArrayList<Task> taskList = new ArrayList<Task>();
 
-	@Override
-	public List<Task> findAll() {
+        for (Map<String,Object> item : resultList){
+            Task task = new Task();
+            task.setId((int)item.get("id"));
+            task.setUserId((int)item.get("userId"));
+            task.setTypeId((int)item.get("typeId"));
+            task.setTitle((String)item.get("title"));
+            task.setDetail((String)item.get("detail"));
+            task.setDeadline(((Timestamp)item.get("deadline")).toLocalDateTime());
+            TaskType type = new TaskType();
+            type.setId((int)item.get("type_id"));
+            type.setType((String)item.get("type"));
+            type.setComment((String)item.get("comment"));
 
-		String sql = "SELECT task.id, user_id, type_id, title, detail, deadline, "
-				+ "type, comment FROM task "
-				+ "INNER JOIN task_type ON task.type_id = task_type.id";
+            task.setTaskType(type);
 
-		//削除してください
+            taskList.add(task);
+        }
+        return taskList;
+     }
 
-		//タスク一覧をMapのListで取得
-		List<Map<String, Object>> resultList = null;
+     @Override
+     public Optional<Task> findById(int id){
+        Map<String, Object> map = jdbcTemplate.queryForMap(
+            "SELECT * FROM task INNER JOIN task_type ON task.type_id = task_type.id WHERE task.id = ?",
+            id);//?の部分に第二引数以降のデータが入る→プリペアードステートメントという。SQLインジェクション対策
+        
+        Task task = new Task();
+        task.setId((int)map.get("id"));
+        task.setUserId((int)map.get("userId"));
+        task.setTypeId((int)map.get("typeId"));
+        task.setTitle((String)map.get("title"));
+        task.setDetail((String)map.get("detail"));
+        task.setDeadline(((Timestamp)map.get("deadline")).toLocalDateTime());
 
-		//return用の空のListを用意
-		List<Task> list = null;
+        TaskType type = new TaskType();
+        type.setId((int)map.get("type_id"));
+        type.setType((String)map.get("type"));
+        type.setComment((String)map.get("comment"));
 
-		//二つのテーブルのデータをTaskにまとめる
-		for(Map<String, Object> result : resultList) {
+        task.setTaskType(type);
 
-			Task task = new Task();
-			task.setId((int)result.get("id"));
-			task.setUserId((int)result.get("user_id"));
-			task.setTypeId((int)result.get("type_id"));
-			task.setTitle((String)result.get("title"));
-			task.setDetail((String)result.get("detail"));
-			task.setDeadline(((Timestamp) result.get("deadline")).toLocalDateTime());
+        Optional<Task> taskOpt = Optional.ofNullable(task);
+        
+        return taskOpt;
 
-			TaskType type = new TaskType();
-			type.setId((int)result.get("type_id"));
-			type.setType((String)result.get("type"));
-			type.setComment((String)result.get("comment"));
+     }
 
-			//TaskにTaskTypeをセット
+     @Override
+     public void insert(Task task){
+        jdbcTemplate.update(
+            "INSERT INTO Task(userId, typeId, taskType, title, detail, deadline) VALUES(?, ?, ?, ?, ?, ?)",
+            task.getUserId(),task.getTypeId(),task.getTaskType(),task.getTitle(),task.getDetail(),task.getDeadline());
+     }
 
-			list.add(task);
-		}
-		return list;
-	}
+     @Override
+    //  updateするデータがなかった場合0を返すため返り値の型はintになる（0だったら例外処理とかを実装できる）
+     public int update(Task task){
+        return jdbcTemplate.update(
+            "UPDATE task SET type_id = ?, title = ?, detail = ?, deadline = ? WHERE id = ?",
+            task.getTypeId(),task.getTitle(),task.getDetail(),task.getDeadline());
+     }
 
-	@Override
-	public Optional<Task> findById(int id) {
-		String sql = "SELECT task.id, user_id, type_id, title, detail, deadline, "
-				+ "type, comment FROM task "
-				+ "INNER JOIN task_type ON task.type_id = task_type.id "
-				+ "WHERE task.id = ?";
+     @Override
+     public int deleteById(int id){
+        return jdbcTemplate.update("DELETE FROM task WHERE id = ?", id);
+     }
 
-		//タスクを一件取得
-		Map<String, Object> result = null;
-
-		Task task = new Task();
-		task.setId((int)result.get("id"));
-		task.setUserId((int)result.get("user_id"));
-		task.setTypeId((int)result.get("type_id"));
-		task.setTitle((String)result.get("title"));
-		task.setDetail((String)result.get("detail"));
-		task.setDeadline(((Timestamp) result.get("deadline")).toLocalDateTime());
-
-		TaskType type = new TaskType();
-		type.setId((int)result.get("type_id"));
-		type.setType((String)result.get("type"));
-		type.setComment((String)result.get("comment"));
-		task.setTaskType(type);
-
-		//削除してください
-		Optional<Task> taskOpt = null;
-
-		//taskをOptionalでラップする
-
-		return taskOpt;
-	}
-
-	@Override
-	public void insert(Task task) {
-		jdbcTemplate.update("INSERT INTO task(user_id, type_id, title, detail, deadline) VALUES(?, ?, ?, ?,?)",
-				 task.getUserId(), task.getTypeId(), task.getTitle(), task.getDetail(), task.getDeadline() );
-	}
-
-	@Override
-	public int update(Task task) {
-		return jdbcTemplate.update("UPDATE task SET type_id = ?, title = ?, detail = ?,deadline = ? WHERE id = ?",
-				task.getTypeId(), task.getTitle(), task.getDetail(), task.getDeadline(), task.getId() );
-	}
-
-	@Override
-	public int deleteById(int id) {
-		return jdbcTemplate.update("DELETE FROM task WHERE id = ?", id);
-	}
-
-	@Override
-	public List<Task> findByType(int typeId) {
-		//2-1 指定したtype_idと一致するタスクのリストを取得するためのSQLを記述する
-		String sql = null;
-
-		//2-2 SQLとtypeIdを渡し、タスク一覧をMapのListで取得する
-		List<Map<String, Object>> resultList = null;
-
-		//return用の空のListを用意
-		List<Task> list = new ArrayList<>();
-
-		//二つのテーブルのデータをTaskにまとめる
-		for(Map<String, Object> result : resultList) {
-
-			Task task = new Task();
-			task.setId((int)result.get("id"));
-			task.setUserId((int)result.get("user_id"));
-			task.setTypeId((int)result.get("type_id"));
-			task.setTitle((String)result.get("title"));
-			task.setDetail((String)result.get("detail"));
-			task.setDeadline(((Timestamp) result.get("deadline")).toLocalDateTime());
-
-			TaskType type = new TaskType();
-			type.setId((int)result.get("type_id"));
-			type.setType((String)result.get("type"));
-			type.setComment((String)result.get("comment"));
-			task.setTaskType(type);
-
-			list.add(task);
-		}
-		return list;
-	}
+     @Override
+     public List<Task> findByType(int typeId){
+        return null;
+     }
 
 }
