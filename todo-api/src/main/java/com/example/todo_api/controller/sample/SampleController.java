f0568e0355b6e131ef80a2b8c15b64870d87dfd5
kosaka_shota <shiyota_kosaka@mail.toyota.co.jp>
2025-03-14 08:34:44 +0900

    add repository

--- todo-api/src/main/java/com/example/todo_api/controller/sample/SampleController.java
+++ todo-api/src/main/java/com/example/todo_api/controller/sample/SampleController.java
@@ -9,7 +9,6 @@ import java.time.LocalDateTime;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
--- todo-api/src/main/java/com/example/todo_api/service/sample/SampleService.java
+++ todo-api/src/main/java/com/example/todo_api/service/sample/SampleService.java
@@ -2,11 +2,20 @@ package com.example.todo_api.service.sample;
 
 import org.springframework.stereotype.Service;
 
+import com.example.todo_api.repository.sample.SampleRecord;
+import com.example.todo_api.repository.sample.SampleRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class SampleService {
 
+    private final SampleRepository repository;
+
     public SampleEntity find(){
-        return new SampleEntity("Hello");
+        SampleRecord record = repository.select();
+        return new SampleEntity(record.getContent());
     }
 
 
